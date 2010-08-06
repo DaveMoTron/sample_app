9e3f17dd21707246d21cd712f03926c4f039aeb7
TheDav3 <dave@rubyx.com>
2010-08-06 11:48:53 +1000

    Adding stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #Return a correctly formatted title if no @title variable set
+  def title
+    base_title="Rails Sample App"
+      if @title.nil?
+        base_title
+      else
+        "#{base_title} | #{@title}"
+      end
+  end
+  
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+
+before(:each) do
+  @base_title = "Rails Sample App | "
+end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +17,7 @@ describe PagesController do
       it 'should have the right title' do
         get 'home'
         response.should have_selector("title",
-                  :content => "Sample App | Home")
+                  :content => @base_title + "Home")
       end
     
   end
@@ -26,7 +31,7 @@ describe PagesController do
      it 'should have the right title' do
         get 'content'
         response.should have_selector("title",
-                  :content => "Sample App | Content")
+                  :content => @base_title + "Content")
       end
     
   end
@@ -40,15 +45,25 @@ describe PagesController do
      it 'should have the right title' do
         get 'about'
         response.should have_selector("title",
-                  :content => "Sample App | About")
+                  :content => @base_title + "About")
       end
   
   end
   
-
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+  end
   
-
-
+     it 'should have the right title' do
+        get 'help'
+        response.should have_selector("title",
+                  :content => @base_title + "Help")
+     end
+  
+end
+  
 
 
 end
