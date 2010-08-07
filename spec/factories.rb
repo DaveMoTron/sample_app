6c48e5cf13fa16ac6d18ac660cba683a9bf33acc
TheDav3 <dave@rubyx.com>
2010-08-07 14:56:40 +1000

    Finished user model

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+#By using the symbol ":User", we get Factory Girl to simulate the User model
+
+Factory.define :user do |user|
+  user.name                   "David Cruikshank"
+  user.email                  "david@test.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
